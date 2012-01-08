136c9d4168c1f936abeb8490df5feb79cf9df85e
John Murray <john.m.murray786@gmail.com>
2012-01-08 01:09:01 -0500

    Renamed Common to Support, added Settings
    I added a settings class that will mock Sinatra's settings. In Sinatra it's
    really freaking handy and makes for a nice API. I'm striving for the same
    thing here although I didn't take the time to dig through Sinatra's source
    code (or perhaps that piece is in Rack)

---
+++ generator/lib/Support.rb
@@ -0,0 +1,35 @@
+
+
+module INFLUENTIAL
+
+  class Settings
+    def initialize
+      @settings = {}
+    end
+
+    def []= k, v
+      @settings[k] = v
+      self.class.instance_eval do
+        define_method(k.to_sym) do
+          begin
+            @settings[k].call
+          rescue NoMethodError
+            @settings[k]
+          end
+        end
+      end
+    end
+  end
+
+
+  @settings = Settings.new
+
+  def self.set k, v
+    @settings[k] = v
+  end
+
+  def self.settings
+    @settings
+  end
+
+end
