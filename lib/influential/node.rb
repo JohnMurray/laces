be383be3b87a4720b6db5a2db84e711a9c24f339
John Murray <me@johnmurray.io>
2013-02-17 20:52:53 -0500

    Updating Documentation
    Updated documentation for the Node class to better explain some
    of the uses and properties of how it is linked to other nodes.

--- lib/influential/node.rb
+++ lib/influential/node.rb
@@ -4,7 +4,17 @@ module INFLUENTIAL
 
   ##----
   ## Node to be used in the graph. This contains all the links to other nodes
-  ## and also holds meta-data for each node (gem).
+  ## and also holds meta-data for each node.
+  ##
+  ## It may be important to note that a node is representative of a Ruby gem
+  ## and all of it's data and links are relevant to the gem meta-data and
+  ## dependencies.
+  ## 
+  ## Bi-Directional Links
+  ##   The nodes link to each other bi-directionally such that if gem A depends
+  ##   on gem B and C, then those gems will be referenced within gem A's
+  ##   'dependencies' array. However, if gem D and E are both dependent upon
+  ##   A, then references to D and E will be found in A's references array.
   ##----
   class Node
     
@@ -14,8 +24,8 @@ module INFLUENTIAL
     def initialize(opts = {})
       options = {
         :name         => nil,
-        :dependencies => [],  #this nodes dependencies
-        :references   => [],  #which gems list me (this gem) as a dependency
+        :dependencies => [],  # this node's dependencies
+        :references   => [],  # which gems list me (this gem) as a dependency
         :weight       => 0,
         :gem_spec     => nil
       }.merge opts
